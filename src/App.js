1bb8a2e3b523b15729a85df5dc6ec143864a239b
adeelnazeer <adeelcomsats070@gmail.com>
2019-12-09 16:21:18 +0500

    homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
-
-export default()=>{
-  return(
-    <>
-             
-    </>
-
+import React from 'react';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Sidenav from '../src/Components/SideNav/Sidenav'
+function App() {
+  return (
+    <div className="App">
+      <Sidenav/>
+    </div>
   );
 }
+
+export default App;
