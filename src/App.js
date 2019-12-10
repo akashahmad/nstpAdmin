a61d232d0965677dc8b412bd5b8f3174f543ecff
sardar-rzubair <zubiq143@gmail.com>
2019-12-10 23:56:51 +0500

    lagin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import LOGIN from './Container/login/login'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default()=>{
   return(
     <>
+    <LOGIN/>
              
     </>
 
