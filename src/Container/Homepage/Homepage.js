9629407d3c606cf7b4d49324c84c01b7f79968f8
unknown <ms335833@gmail.com>
2019-12-15 12:21:38 +0500

    Gold Shop Project

--- src/Container/Homepage/Homepage.js
+++ src/Container/Homepage/Homepage.js
@@ -2,7 +2,6 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Section1 from '../../Components/Section1/section1'
 import Section2 from '../../Components/Section2/section2'
-import Section3 from '../../Components/Section3/section3'
 import '../../assects/style/common.css'
 
 export default ()=>{
